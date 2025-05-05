3caab533fde823f6f140a28be9c5f81ea4419a82
windymelt <windymelt@3qe.us>
2025-05-06 00:33:11 +0900

    separator

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 import scala.scalanative.unsafe
 import scala.scalanative.unsigned.* // for .toCSize
 import scala.scalanative.libc.stdio.FILE
+import scala.scalanative.posix.string.strlen
 import scala.scalanative.unsafe.Zone
 import scala.scalanative.unsafe.CQuote
 import scala.util.boundary
@@ -16,18 +17,28 @@ object Main {
   @main
   def run(
       @arg(name = "squeeze-blank", short = 's') squeeze: Flag,
+      @arg(short = 'S') separator: String = "",
       @arg(positional = true) infiles: Leftover[String]
   ): Unit =
     infiles.value match
       case Seq() =>
-        cats("-" :: Nil, squeeze.value)
+        cats("-" :: Nil, squeeze.value, separator)
       case fs =>
-        cats(fs.toList, squeeze.value)
+        cats(fs.toList, squeeze.value, separator)
 
   def usage(): Unit = println("not implemented yet")
 
-  def cats(infiles: List[String], squeeze: Boolean): Unit =
+  def cats(
+      infiles: List[String],
+      squeeze: Boolean,
+      separator: String = ""
+  ): Unit =
+    var copiedFileCount = 0
+    val fileCount = infiles.length
     Zone {
+      val separatorCStr = unsafe.toCString(separator)
+      extractEscapeSequence(separatorCStr, strlen(separatorCStr))
+
       boundary {
         val stdout = scalanative.libc.stdio.stdout
         if (stdout == null) then
@@ -55,6 +66,15 @@ object Main {
             copy(inStream, stdout, buf, bufSize, squeeze)
 
             scalanative.libc.stdio.fclose(inStream)
+
+            if (copiedFileCount < fileCount - 1) then
+              scalanative.posix.stdio.fprintf(
+                stdout,
+                c"%s",
+                separatorCStr
+              )
+
+            copiedFileCount += 1
         }
 
         scalanative.libc.stdio.fclose(stdout)
@@ -111,4 +131,49 @@ object Main {
 
       writePos
     }
+
+    /** Extracts the escape sequence from the buffer. For example, "foo\n"
+      * (literally) will be converted to "foo(line break)". This is used to
+      * convert the escape sequence to a string. XXX: If we use BSD, we can use
+      * strunvis() to convert the escape
+      *
+      * @param buf
+      * @param bufSize
+      * @return
+      */
+  def extractEscapeSequence(
+      buf: unsafe.CString,
+      bufSize: unsafe.CSize
+  ): unsafe.CSize =
+    val map = Map(
+      'n'.toByte -> '\n',
+      't'.toByte -> '\t',
+      'r'.toByte -> '\r'
+    )
+    // scan escape sequence and replace
+    var writePos = 0.toCSize
+    var readPos = 0.toCSize
+
+    while (readPos < bufSize) do
+      val currentChar = buf(readPos)
+
+      if (currentChar == '\\') then
+        readPos += 1.toCSize
+        val escapeChar = buf(readPos)
+        map.get(escapeChar) match
+          case Some(replacement) =>
+            buf(writePos) = replacement.toByte
+            writePos += 1.toCSize
+          case None =>
+            buf(writePos) = currentChar
+            writePos += 1.toCSize
+      else
+        buf(writePos) = currentChar
+        writePos += 1.toCSize
+
+      readPos += 1.toCSize
+
+    // null terminate the string
+    buf(writePos) = 0.toByte
+    writePos
 }
